6e088f45c7b464519d95bb9d4a48e88bed56b921
Jesper Carlsen <lordfe@kreakageklubben.org>
2019-06-30 12:24:50 +0200

    Update score_handler.js

--- score_handler.js
+++ score_handler.js
@@ -1,5 +1,14 @@
 let kills[];
 
+const mKillTemplate = JSON.stringify({
+    _id :  0,
+    char_id: 0,
+    is_headshot: 0,
+    timestamp: 0
+   // name:  'Unknown',
+
+});
+
 function addKill(data){
     
 }
